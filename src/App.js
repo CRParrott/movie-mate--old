6fea9d39ea7b38c84b5c0df7ff306a72a409de17
tassoni13 <tassoni13@gmail.com>
2021-09-08 20:49:54 -0400

    Set up file directory

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+//import local files
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  //function to render app goes here
+
+  export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,1 @@
+//Homepage data

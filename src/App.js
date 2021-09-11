0afb62f2e0c46b5035a578ff4c689d3ea8d0b9c9
tassoni13 <tassoni13@gmail.com>
2021-09-11 14:03:32 -0400

    added signup and login routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+  } from "@apollo/client";  
 import { setContext } from '@apollo/client/link/context';
 
 //import local files
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Signup from './pages/Signup'
+
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
+    uri: 'localhost:3001/graphql',
   });
   
   const authLink = setContext((_, { headers }) => {
@@ -25,10 +30,25 @@ const httpLink = createHttpLink({
   });
   
   const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    uri: httpLink,
     cache: new InMemoryCache(),
   });
 
   //function to render app goes here
+  function App () {
+      return (
+          <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component ={Login} />
+                        <Route exact path ="/signup" component={Signup} />
+                    </Switch>
+                </div>
+            </Router>
+        </ApolloProvider>
+      );
+  }
 
   export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,14 @@
-//Homepage data
+import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <Login />
+            <Signup />
+        </div>
+    );
+};
+
+export default Home;
